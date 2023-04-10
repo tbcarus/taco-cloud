9afa36c548703a7c3c2855a689b63619b69adb06
tbcarus <70680359+tbcarus@users.noreply.github.com>
2023-04-10 08:04:51 +0300

    ch02 add controllers and views

--- src/main/java/tacos/TacoCloudApplication.java
+++ src/main/java/tacos/TacoCloudApplication.java
@@ -1,4 +1,4 @@
-package sia.tacos;
+package tacos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/tacos/TacoCloudApplicationTests.java
+++ src/test/java/tacos/TacoCloudApplicationTests.java
@@ -1,4 +1,4 @@
-package sia.tacos;
+package tacos;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
