9b1cbf7da39b07a8de98f2a088a9cf104dac16ac
tbcarus <70680359+tbcarus@users.noreply.github.com>
2023-04-11 07:36:48 +0300

    ch02 another HomeController declaration

--- src/test/java/tacos/HomeControllerTest.java
+++ src/test/java/tacos/HomeControllerTest.java
@@ -10,10 +10,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
-import tacos.HomeController;
 
 @SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
-@WebMvcTest(HomeController.class)
+@WebMvcTest()
 class HomeControllerTest {
 
     @Autowired
