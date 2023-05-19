adeafe9c977cd18fc473f09f2d8f1e7c9f501e91
tbcarus <70680359+tbcarus@users.noreply.github.com>
2023-05-19 22:21:06 +0300

    ch08 IngredientRestController save and delete

--- curl.md
+++ curl.md
@@ -1,2 +1,11 @@
 curl http://localhost:8080/api/tacos?recent
+
 curl http://localhost:8080/api/tacos/100004
+
+curl -X DELETE localhost:8080/api/ingredients/GRBF --user user:pass
+
+curl localhost:8080/api/ingredients
+
+curl -X POST localhost:8080/api/ingredients -H"Content-type: application/json" –d"{\"id\":\"FISH\",\"name\":\"Stinky Fish\", \"type\":\"PROTEIN\"}"
+
+curl -X DELETE localhost:8080/api/ingredients/FISH
--- src/main/java/tacos/web/rest/IngredientRestController.java
+++ src/main/java/tacos/web/rest/IngredientRestController.java
@@ -1,10 +1,10 @@
 package tacos.web.rest;
 
+import jakarta.annotation.security.RolesAllowed;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
 import tacos.model.Ingredient;
 import tacos.repository.IngredientRepository;
 
@@ -23,6 +23,20 @@ public class IngredientRestController {
   @GetMapping
   public Iterable<Ingredient> allIngredients() {
     return ingredientRepository.findAll();
+  }
+
+  @PostMapping
+  @ResponseStatus(HttpStatus.CREATED)
+  @PreAuthorize("hasRole('USER')")
+  public Ingredient saveIngredient(@RequestBody Ingredient ingredient) {
+    return ingredientRepository.save(ingredient);
+  }
+
+  @DeleteMapping("/{id}")
+  @ResponseStatus(HttpStatus.NO_CONTENT)
+//  @PreAuthorize("hasRole('ADMIN')")
+  public void deleteIngredient(@PathVariable String id) {
+    ingredientRepository.deleteById(id);
   }
 
 }
