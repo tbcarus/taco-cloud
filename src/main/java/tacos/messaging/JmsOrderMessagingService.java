a9de6d0ea8e0d1be90ad46063b400bf425c3dcb8
tbcarus <70680359+tbcarus@users.noreply.github.com>
2023-05-21 20:14:41 +0300

    ch09 JMS sending messages

---
+++ src/main/java/tacos/messaging/JmsOrderMessagingService.java
@@ -0,0 +1,28 @@
+package tacos.messaging;
+
+import jakarta.jms.JMSException;
+import jakarta.jms.Message;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.stereotype.Service;
+import tacos.model.TacoOrder;
+
+@Service
+public class JmsOrderMessagingService implements OrderMessagingService {
+    private JmsTemplate jms;
+
+    @Autowired
+    public JmsOrderMessagingService(JmsTemplate jms) {
+        this.jms = jms;
+    }
+    @Override
+    public void sendOrder(TacoOrder order) {
+//        jms.send("tacocloud.order.queue", session -> session.createObjectMessage(order));
+        jms.convertAndSend("tacocloud.order.queue", order, message -> addOrderSource(message));
+    }
+
+    private Message addOrderSource(Message message) throws JMSException {
+        message.setStringProperty("X_ORDER_SOURCE", "WEB");
+        return message;
+    }
+}
