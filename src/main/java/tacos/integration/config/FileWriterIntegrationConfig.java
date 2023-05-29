5a99dda2d90f32911f76d3cc48cacb62fa5b99a0
tbcarus <70680359+tbcarus@users.noreply.github.com>
2023-05-29 20:04:01 +0300

    ch10 Spring integration XML config

---
+++ src/main/java/tacos/integration/config/FileWriterIntegrationConfig.java
@@ -0,0 +1,26 @@
+package tacos.integration.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.context.annotation.Profile;
+import org.springframework.integration.annotation.ServiceActivator;
+import org.springframework.integration.annotation.Transformer;
+import org.springframework.integration.core.GenericTransformer;
+import org.springframework.integration.dsl.IntegrationFlow;
+import org.springframework.integration.dsl.IntegrationFlows;
+import org.springframework.integration.dsl.MessageChannels;
+import org.springframework.integration.file.FileWritingMessageHandler;
+import org.springframework.integration.file.dsl.Files;
+import org.springframework.integration.file.support.FileExistsMode;
+
+import java.io.File;
+
+@Configuration
+public class FileWriterIntegrationConfig {
+
+  @Profile("xmlconfig")
+  @Configuration
+  @ImportResource("filewriter-config.xml")
+  public static class XmlConfiguration {}
+}
