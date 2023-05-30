17f9aebcdde54468756602ec68a61adcb771108b
tbcarus <70680359+tbcarus@users.noreply.github.com>
2023-05-30 22:18:08 +0300

    ch10 Spring integration java config

--- src/main/java/tacos/integration/config/FileWriterIntegrationConfig.java
+++ src/main/java/tacos/integration/config/FileWriterIntegrationConfig.java
@@ -19,8 +19,59 @@ import java.io.File;
 @Configuration
 public class FileWriterIntegrationConfig {
 
-  @Profile("xmlconfig")
-  @Configuration
-  @ImportResource("filewriter-config.xml")
-  public static class XmlConfiguration {}
+    @Profile("xmlconfig")
+    @Configuration
+    @ImportResource("classpath:/filewriter-config.xml")
+    public static class XmlConfiguration {
+    }
+
+    @Profile("javaconfig")
+    @Bean
+    @Transformer(inputChannel = "textInChannel", outputChannel = "fileWriterChannel")
+    public GenericTransformer<String, String> upperCaseTransformer() {
+        return text -> text.toUpperCase();
+    }
+
+    @Profile("javaconfig")
+    @Bean
+    @ServiceActivator(inputChannel = "fileWriterChannel")
+    public FileWritingMessageHandler fileWriter() {
+        FileWritingMessageHandler handler = new FileWritingMessageHandler(new File("tmp/sia6/files"));
+        handler.setExpectReply(false);
+        handler.setFileExistsMode(FileExistsMode.APPEND);
+        handler.setAppendNewLine(true);
+        return handler;
+    }
+
+    //
+    // DSL Configuration
+    //
+    @Profile("javadsl")
+    @Bean
+    public IntegrationFlow fileWriterFlow() {
+        return IntegrationFlows
+                .from(MessageChannels.direct("textInChannel"))
+                .<String, String>transform(t -> t.toUpperCase())
+                .handle(Files
+                        .outboundAdapter(new File("tmp/sia6/files"))
+                        .fileExistsMode(FileExistsMode.APPEND)
+                        .appendNewLine(true))
+                .get();
+    }
+
+  /*
+  @Bean
+  public IntegrationFlow fileWriterFlow() {
+    return IntegrationFlows
+        .from(MessageChannels.direct("textInChannel"))
+        .<String, String>transform(t -> t.toUpperCase())
+        .channel(MessageChannels.direct("FileWriterChannel"))
+        .handle(Files
+            .outboundAdapter(new File("/tmp/sia6/files"))
+            .fileExistsMode(FileExistsMode.APPEND)
+            .appendNewLine(true))
+        .get();
+  }
+   */
+
 }
